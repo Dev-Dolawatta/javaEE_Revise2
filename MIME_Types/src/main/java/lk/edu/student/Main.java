eaf2a5ad5a19cb0cea47b3721afd655258ccaa7e
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-08-13 16:29:33 +0530

    reading a plain text from request body

---
+++ MIME_Types/src/main/java/lk/edu/student/Main.java
@@ -0,0 +1,26 @@
+package lk.edu.student;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@WebServlet("/mime")
+public class Main extends HttpServlet{
+    //1
+    ////read text/plain data from http request body  - raw
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String body = new BufferedReader(new InputStreamReader(req.getInputStream())).lines().collect(Collectors.joining("\n"));
+        resp.setContentType("text/plain");
+        resp.getWriter().write(body);//send it back  as a text in the response
+
+
+    }
+}
