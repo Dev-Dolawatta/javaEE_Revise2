a86c92e0035397793567a8abf1200ecf7b4eef86
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-08-13 16:50:00 +0530

    reading a xw form urlencoded data

--- MIME_Types/src/main/java/lk/edu/student/Main.java
+++ MIME_Types/src/main/java/lk/edu/student/Main.java
@@ -13,14 +13,24 @@ import java.util.stream.Collectors;
 
 @WebServlet("/mime")
 public class Main extends HttpServlet{
-    //1
-    ////read text/plain data from http request body  - raw
+//    //1
+//    ////read text/plain data from http request body  - raw
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String body = new BufferedReader(new InputStreamReader(req.getInputStream())).lines().collect(Collectors.joining("\n"));
+//        resp.setContentType("text/plain");
+//        resp.getWriter().write(body);//send it back  as a text in the response
+//
+//
+//    }
+
+    //2
+    //// reading a x_w_form_urlencoded data
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String body = new BufferedReader(new InputStreamReader(req.getInputStream())).lines().collect(Collectors.joining("\n"));
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
         resp.setContentType("text/plain");
-        resp.getWriter().write(body);//send it back  as a text in the response
-
-
+        resp.getWriter().write("name: " + name + "\naddress: " + address + "\n");
     }
 }
